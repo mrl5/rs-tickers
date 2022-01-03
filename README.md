8eea8bd7c622735872e9c0f72b5ae9202adff5c9
mrl5 <31549762+mrl5@users.noreply.github.com>
2022-01-03 23:41:36 +0100

    feat(cli): read lines from file provided as an CLI arg

---
+++ README.md
@@ -0,0 +1,9 @@
+# rs-tickers
+
+Gets stock quotes for provided tickers
+
+
+## Usage
+```
+cargo run src/main.rs path/to/tickers.json
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    std::process::exit(match run_app(&args) {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("error: {}", err);
+            1
+        }
+    });
+}
+
+fn run_app(args: &Vec<String>) -> Result<(), io::Error> {
+    if args.len() > 2 {
+        let filename = &args[2];
+        read_from_file(filename)
+    } else {
+        Ok(())
+    }
+}
+
+fn read_from_file<P>(filename: P) -> Result<(), io::Error>
+where P: AsRef<Path>, {
+    for line in read_lines(filename)? {
+        if let Ok(l) = line {
+            println!("{}", l);
+        }
+    }
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
