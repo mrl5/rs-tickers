5d1a029d68e47f1f68eff064954883e684129bbf
mrl5 <31549762+mrl5@users.noreply.github.com>
2022-01-06 18:29:37 +0100

    perf(http): support gzip ecoding and auto-decompress

--- src/http.rs
+++ src/http.rs
@@ -4,12 +4,14 @@ pub fn get_client() -> Result<reqwest::blocking::Client, reqwest::Error> {
     let timeout = Duration::from_secs(10);
     let is_verbose = true;
     let tcp_keepalive = Duration::from_secs(180);
+    let use_gzip = true;
     let user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36";
 
     reqwest::blocking::Client::builder()
         .timeout(timeout)
         .connection_verbose(is_verbose)
         .tcp_keepalive(tcp_keepalive)
+        .gzip(use_gzip)
         .user_agent(user_agent)
         .build()
 }
