89f5fa84f12d7b9497ecfc659443f36c88416efa
mrl5 <31549762+mrl5@users.noreply.github.com>
2022-01-07 21:58:18 +0100

    test(lib): it should serialize from json

--- crates/rs-tickers/src/lib.rs
+++ crates/rs-tickers/src/lib.rs
@@ -3,9 +3,18 @@ pub mod stock_quote;
 
 #[cfg(test)]
 mod tests {
+    use crate::stock_quote::StockQuote;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_should_serialize_from_json() {
+        let test_input = [
+            r#"{"symbol":"aapl","source":"yahoo"}"#,
+            r#"{"symbol":"cdr","source":"stooq"}"#,
+            r#"{"symbol":"gbpusd","source":"yahoo","ticker":"gbpusd=x"}"#,
+        ];
+        for json in test_input {
+            let sq: Result<StockQuote, serde_json::error::Error> = serde_json::from_str(json);
+            assert!(sq.is_ok());
+        }
     }
 }
