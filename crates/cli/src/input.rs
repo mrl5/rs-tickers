5e4ecc096083222b83affa8058bbcb65dac35c9e
mrl5 <31549762+mrl5@users.noreply.github.com>
2022-01-07 20:52:27 +0100

    feat(cli): richer CLI with --help and --version

--- crates/cli/src/input.rs
+++ crates/cli/src/input.rs
@@ -1,26 +1,33 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use once_cell::sync::Lazy;
 use atty::Stream;
+use once_cell::sync::Lazy;
+use super::CliOptions;
 
 static STDIN: Lazy<io::Stdin> = Lazy::new(io::stdin);
 
 pub fn get_lines(
-    args: &Vec<String>,
-) -> Result<either::Either<io::Lines<io::BufReader<File>>, io::Lines<io::StdinLock>>, io::Error> {
+    opts: CliOptions,
+) -> Result<
+    either::Either<io::Lines<io::BufReader<File>>, io::Lines<io::StdinLock<'static>>>,
+    io::Error,
+> {
     let lines;
 
-    if args.len() > 1 {
-        let file = File::open(&args[1])?;
-        let l = io::BufReader::new(file).lines();
-        lines = either::Either::Left(l);
-    } else {
-        if atty::is(Stream::Stdin) {
-            return Err(io::Error::new(io::ErrorKind::Other, "stdin not redirected"));
+    match opts.json_path {
+        Some(p) => {
+            let file = File::open(p)?;
+            let l = io::BufReader::new(file).lines();
+            lines = either::Either::Left(l);
         }
+        None => {
+            if atty::is(Stream::Stdin) {
+                return Err(io::Error::new(io::ErrorKind::Other, "stdin not redirected"));
+            }
 
-        let l = STDIN.lock().lines();
-        lines = either::Either::Right(l);
+            let l = STDIN.lock().lines();
+            lines = either::Either::Right(l);
+        }
     }
 
     Ok(lines)
